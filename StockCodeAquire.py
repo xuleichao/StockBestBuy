299ac321e2d564699870ef129b0f7578ec1224a2
xuleichao <1205448156@qq.com>
2019-03-11 22:11:35 +0800

    获取页面数据

---
+++ StockCodeAquire.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import urllib.request
+
+from bs4 import BeautifulSoup as bsp
+
+def get_soup(url):
+    """获得指定url 的HTML，并返回soup"""
+    html_data = urllib.request.urlopen(url).read()
+    soup = bsp(html_data, 'lxml')
+    return soup
+
+if __name__ == '__main__':
+    url = 'http://quote.eastmoney.com/stocklist.html'
+    soup = get_soup(url)
+    lis = soup.find_all('li')
+    for i in lis:
+        print(i)
+
