76877f2688249d271241f9cbd5f619da4d2c6763
xuleichao <1205448156@qq.com>
2019-03-15 23:22:52 +0800

    正则获得股票代码

--- StockCodeAquire.py
+++ StockCodeAquire.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 import numpy as np
 import urllib.request
@@ -14,11 +15,22 @@ def li_filter(li):
     if '(' in li.text:
         return li
 
+def regex_get_stock_code(text):
+    '''传入股票和股票代码-京东(00001)'''
+    reg = '\(([0-9]+)\)'
+    code = re.findall(reg, text)
+    return code
+
 if __name__ == '__main__':
     url = 'http://quote.eastmoney.com/stocklist.html'
     soup = get_soup(url)
     lis = soup.find_all('li')
+    count = 0
     for i in lis:
         if li_filter(i):
             print(i.text)
+            a = regex_get_stock_code(i.text)
+            print(a)
+            count += 1
+    print(count)
 
