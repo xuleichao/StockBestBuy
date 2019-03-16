c27eee12c3f22e2eec055b258ab0a46eac9e775f
xuleichao <1205448156@qq.com>
2019-03-16 10:22:12 +0800

    正则获得股票代码

--- StockCodeAquire.py
+++ StockCodeAquire.py
@@ -1,8 +1,6 @@
 import re
-import pandas as pd
-import numpy as np
 import urllib.request
-
+from pandas import DataFrame
 from bs4 import BeautifulSoup as bsp
 
 def get_soup(url):
@@ -19,18 +17,30 @@ def regex_get_stock_code(text):
     '''传入股票和股票代码-京东(00001)'''
     reg = '\(([0-9]+)\)'
     code = re.findall(reg, text)
-    return code
+    # 如果监测到文本中包含股票代码
+    if code:
+        # 获取股票的名称
+        name = text\
+            .replace('(', "")\
+            .replace(')', "")\
+            .replace(code[0], '')
+        return name, code[0]
 
 if __name__ == '__main__':
     url = 'http://quote.eastmoney.com/stocklist.html'
     soup = get_soup(url)
     lis = soup.find_all('li')
-    count = 0
+    rslt = []
     for i in lis:
         if li_filter(i):
-            print(i.text)
-            a = regex_get_stock_code(i.text)
-            print(a)
-            count += 1
-    print(count)
+            a = regex_get_stock_code(i.text)#从抓取的文本中获取股票代码信息
+            if a:
+                rslt.append(a)
+    # 数据转换为dataframe表格
+    stock_df = DataFrame(rslt, columns=['name', 'code'])
+    # 将数据写入文件
+    with open('./dataSets/stock_codes.txt', 'w', encoding='utf-8') as f:
+        for i in rslt:
+            f.write('\t'.join(i)+'\n')
+
 
